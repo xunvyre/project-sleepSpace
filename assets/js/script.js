dcfd4b3af64b23277ea32ae2b29a05b23a65b9a1
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-07-21 10:01:29 -0400

    fetch-moonphase/getUserLocation done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-const clearBtn = document.querySelector("#borrowing");
+//const clearBtn = document.querySelector("#borrowing");
 
 //function to get moonphase
-var getMoonApi = function () {
-  var moonApi =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=28.53833&lon=-81.378876&exclude=current,minutely,hourly,alerts&appid=be26d6ce167eb956b80efee1a9fcecbd";
+var getMoonApi = function (lat, lon) {
+  var moonApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=be26d6ce167eb956b80efee1a9fcecbd`;
   fetch(moonApi)
     .then((res) => res.json())
     .then((data) => {
@@ -11,14 +10,15 @@ var getMoonApi = function () {
       console.log(moonPhase);
     });
 };
-getMoonApi();
 
 //function that grabs user location (we can add this to any button just used clear as a test)
 clearBtn.addEventListener("click", function () {
   navigator.geolocation.getCurrentPosition((location) => {
     let lat = location.coords.latitude;
     let lon = location.coords.longitude;
+    //gonna pass these off to above function soon
     console.log(lat, lon);
+    getMoonApi(lat, lon);
   });
 });
 
